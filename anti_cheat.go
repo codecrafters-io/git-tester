153a8d225bf5d4968cd5529d5eba42bb1209df47
Paul Kuruvilla <rohitpaulk@gmail.com>
2020-03-23 20:05:16 +0530

    Remove AC

--- anti_cheat.go
+++ anti_cheat.go
@@ -1,45 +1,8 @@
 package main
 
-import "github.com/go-redis/redis"
-import "fmt"
-import "time"
-import "strings"
-
 func antiCheatRunner() StageRunner {
 	return StageRunner{
 		isDebug: false,
-		stages: []Stage{
-			Stage{
-				name:    "AC1",
-				logger:  getQuietLogger("[anticheat] "),
-				runFunc: testCommand,
-			},
-		},
+		stages:  []Stage{},
 	}
-}
-
-func testCommand(executable *Executable, logger *customLogger) error {
-	client := redis.NewClient(&redis.Options{
-		Addr:        "localhost:6379",
-		DialTimeout: 30 * time.Second,
-	})
-	result := client.Info("server")
-	if result.Err() != nil {
-		return nil
-	}
-
-	str, err := result.Result()
-	if err != nil {
-		return nil
-	}
-
-	if !strings.HasPrefix(str, "# Server") {
-		return nil
-	}
-
-	logger.Criticalf("anti-cheat (ac1) failed. ")
-	logger.Criticalf(
-		"Are you sure you aren't running this " +
-			"against the actual Redis?")
-	return fmt.Errorf("anti-cheat (ac1) failed")
 }
