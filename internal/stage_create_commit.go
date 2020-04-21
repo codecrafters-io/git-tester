42c341c3d94e686f5fb8a68c27bfc659b5fa2b2e
Paul Kuruvilla <rohitpaulk@gmail.com>
2020-04-21 15:33:22 +0530

    Use go-git for second-last stage

--- internal/stage_create_commit.go
+++ internal/stage_create_commit.go
@@ -5,6 +5,11 @@ import (
 	"io/ioutil"
 	"path"
 	"strings"
+	"time"
+
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
+	"github.com/go-git/go-git/v5/plumbing/object"
 )
 
 func testCreateCommit(executable *Executable, logger *customLogger) error {
@@ -16,7 +21,9 @@ func testCreateCommit(executable *Executable, logger *customLogger) error {
 	executable.WorkingDir = tempDir
 
 	logger.Debugf("Running git init")
-	runGitCmd(tempDir, "init")
+	if _, err := git.PlainInit(tempDir, false); err != nil {
+		return err
+	}
 
 	logger.Debugf("Creating some files & directories")
 
@@ -34,18 +41,58 @@ func testCreateCommit(executable *Executable, logger *customLogger) error {
 	writeFile(tempDir, path.Join(rootDir2, rootDir2File1))
 
 	logger.Debugf("Running git commit --all")
-	runGitCmd(tempDir, "add", ".")
-	runGitCmd(tempDir, "commit", "--all", "-m", "initial commit")
-	parentCommitSha := strings.TrimSpace(runGitCmd(tempDir, "rev-parse", "@"))
+	repository, err := git.PlainOpen(tempDir)
+	if err != nil {
+		return err
+	}
+
+	w, err := repository.Worktree()
+	if err != nil {
+		return err
+	}
+
+	if err = w.AddGlob("."); err != nil {
+		return err
+	}
+
+	commitHash, err := w.Commit("test2", &git.CommitOptions{
+		Author: &object.Signature{
+			Name:  "test",
+			Email: "test",
+			When:  time.Now(),
+		},
+	})
+	if err != nil {
+		return err
+	}
+	parentCommitSha := commitHash.String()
 
 	logger.Debugf("Creating another file")
 	writeFile(tempDir, path.Join(rootDir2, rootDir2File2))
-	runGitCmd(tempDir, "add", ".")
-	logger.Debugf("Creating a new tree object")
-	treeSha := strings.TrimSpace(runGitCmd(tempDir, "write-tree"))
+
+	if err = w.AddGlob("."); err != nil {
+		return err
+	}
+
+	nextCommitHash, err := w.Commit("test2", &git.CommitOptions{
+		Author: &object.Signature{
+			Name:  "test",
+			Email: "test",
+			When:  time.Now(),
+		},
+	})
+	if err != nil {
+		return err
+	}
+
+	nextCommit, err := repository.CommitObject(nextCommitHash)
+	if err != nil {
+		return err
+	}
+
+	treeSha := nextCommit.TreeHash.String()
 
 	commitMessage := randomString()
-
 	logger.Debugf("Running ./your_git.sh commit-tree <tree_sha> -p <commit_sha> -m <message>")
 	result, err := executable.Run("commit-tree", treeSha, "-p", parentCommitSha, "-m", commitMessage)
 	if err != nil {
@@ -62,23 +109,28 @@ func testCreateCommit(executable *Executable, logger *customLogger) error {
 	}
 
 	logger.Debugf("Running git cat-file commit <sha>")
-	result, err = runGitCmdUnsafe(tempDir, "cat-file", "commit", sha)
+	c, err := repository.CommitObject(plumbing.NewHash(sha))
 	if err != nil {
 		return err
 	}
 
-	if err = assertExitCode(result, 0); err != nil {
-		return err
+	expected, actual := treeSha, c.TreeHash.String()
+	if expected != actual {
+		return fmt.Errorf("Expected %q as tree, got: %q", expected, actual)
 	}
 
-	if err = assertStdoutContains(result, fmt.Sprintf("tree %s", treeSha)); err != nil {
+	parent, err := c.Parent(0)
+	if err != nil {
 		return err
 	}
-	if err = assertStdoutContains(result, fmt.Sprintf("parent %s", parentCommitSha)); err != nil {
-		return err
+	expected, actual = parentCommitSha, parent.Hash.String()
+	if expected != actual {
+		return fmt.Errorf("Expected %q as parent commit, got: %q", expected, actual)
 	}
-	if err = assertStdoutContains(result, commitMessage); err != nil {
-		return err
+
+	expected, actual = commitMessage+"\n", c.Message
+	if expected != actual {
+		return fmt.Errorf("Expected %q as commit message, got: %q", expected, actual)
 	}
 
 	return nil
