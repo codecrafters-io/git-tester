db7a094854644dc3319887a5c7de4d619482ac4d
Paul Kuruvilla <rohitpaulk@gmail.com>
2020-06-30 23:22:15 +0530

    Update timeout to 15 seconds

--- internal/stage_runner.go
+++ internal/stage_runner.go
@@ -92,8 +92,8 @@ func (r StageRunner) Run(executable *Executable) StageRunnerResult {
 		select {
 		case stageErr := <-stageResultChannel:
 			err = stageErr
-		case <-time.After(10 * time.Second):
-			err = fmt.Errorf("timed out, test exceeded 10 seconds")
+		case <-time.After(15 * time.Second):
+			err = fmt.Errorf("timed out, test exceeded 15 seconds")
 		}
 
 		if err != nil {
