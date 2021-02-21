ca68f3a38f74a40473ebc4f2403fd00273dc7dc7
Paul Kuruvilla <rohitpaulk@gmail.com>
2021-02-21 16:33:20 +0530

    remove unused functions

--- internal/stage_helpers.go
+++ internal/stage_helpers.go
@@ -6,22 +6,6 @@ import (
 	"time"
 )
 
-func runGitCmdUnsafe(workingDir string, args ...string) (ExecutableResult, error) {
-	executable := NewExecutable("zit")
-	executable.WorkingDir = workingDir
-	return executable.Run(args...)
-}
-
-func runGitCmd(workingDir string, args ...string) string {
-	executable := NewExecutable("zit")
-	executable.WorkingDir = workingDir
-	result, err := executable.Run(args...)
-	if err != nil {
-		panic(err)
-	}
-	return string(result.Stdout)
-}
-
 var randomWords = []string{
 	"humpty",
 	"dumpty",
