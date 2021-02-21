8841fadeec98332fa3d620b5a72b1a30429cdc02
Paul Kuruvilla <rohitpaulk@gmail.com>
2021-02-21 16:55:09 +0530

    remove redundant stuff

--- internal/tester_definition.go
+++ internal/tester_definition.go
@@ -10,37 +10,37 @@ var testerDefinition = testerutils.TesterDefinition{
 	Stages: []testerutils.Stage{
 		{
 			Slug:     "init",
-			Title:    "Stage 1: Initialize the .git directory",
+			Title:    "Initialize the .git directory",
 			TestFunc: testInit,
 		},
 		{
 			Slug:     "read_blob",
-			Title:    "Stage 2: Read a blob object",
+			Title:    "Read a blob object",
 			TestFunc: testReadBlob,
 		},
 		{
 			Slug:     "create_blob",
-			Title:    "Stage 3: Create a blob object",
+			Title:    "Create a blob object",
 			TestFunc: testCreateBlob,
 		},
 		{
 			Slug:     "read_tree",
-			Title:    "Stage 4: Read a tree object",
+			Title:    "Read a tree object",
 			TestFunc: testReadTree,
 		},
 		{
 			Slug:     "write_tree",
-			Title:    "Stage 5: Write a tree object",
+			Title:    "Write a tree object",
 			TestFunc: testWriteTree,
 		},
 		{
 			Slug:     "create_commit",
-			Title:    "Stage 6: Create a commit object",
+			Title:    "Create a commit object",
 			TestFunc: testCreateCommit,
 		},
 		{
 			Slug:     "clone_repository",
-			Title:    "Stage 7: Clone a repository",
+			Title:    "Clone a repository",
 			TestFunc: testCloneRepository,
 		},
 	},
