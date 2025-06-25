beeb91eb8accf8253b5e45bc8f6f83610b2b8f95
Ryan <ryan-gg@outlook.com>
2025-06-25 16:34:33 +0530

    refactor: remove cloneRepoNew function and integrate its logic directly into cloneRepo for improved clarity

--- debug/cmd/mygit/main.go
+++ debug/cmd/mygit/main.go
@@ -10,6 +10,8 @@ import (
 	"path/filepath"
 	"sort"
 	"time"
+
+	"github.com/go-git/go-git/v5"
 )
 
 type TreeEntry struct {
@@ -409,5 +411,16 @@ func commitTree(args []string) {
 }
 
 func cloneRepo(repoURL, targetDir string) {
-	cloneRepoNew(repoURL, targetDir)
+	fmt.Printf("Cloning into '%s'...\n", targetDir)
+
+	// Use go-git to perform the clone
+	_, err := git.PlainClone(targetDir, false, &git.CloneOptions{
+		URL:      repoURL,
+		Progress: os.Stdout,
+	})
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error cloning repository: %v\n", err)
+		os.Exit(1)
+	}
 }
