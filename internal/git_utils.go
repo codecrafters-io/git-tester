591de8e76848819ac9c8017018177d50dd2237ce
Ryan <ryan-gg@outlook.com>
2025-06-26 11:22:50 +0530

    fix: comment out debug logging in MoveGitToTemp and RestoreGit functions to reduce console output

--- internal/git_utils.go
+++ internal/git_utils.go
@@ -24,17 +24,17 @@ func MoveGitToTemp(logger *logger.Logger) (*GitTempDir, error) {
 		return nil, fmt.Errorf("git executable not found: %v", err)
 	}
 	oldGitDir := path.Dir(oldGitPath)
-	logger.Debugf("Found git executable at: %s", oldGitPath)
+	// logger.Debugf("Found git executable at: %s", oldGitPath)
 
 	tmpGitDir, err := os.MkdirTemp("/tmp", "git-*")
 	if err != nil {
 		return nil, err
 	}
-	logger.Debugf("Created temporary directory for git clone: %s", tmpGitDir)
+	// logger.Debugf("Created temporary directory for git clone: %s", tmpGitDir)
 	tmpGitPath := path.Join(tmpGitDir, "git")
 
 	command := fmt.Sprintf("sudo mv %s %s", oldGitPath, tmpGitDir)
-	logger.Debugf("command: %s", command)
+	// logger.Debugf("command: %s", command)
 	moveCmd := exec.Command("sh", "-c", command)
 	moveCmd.Stdout = os.Stdout
 	moveCmd.Stderr = os.Stderr
@@ -42,7 +42,7 @@ func MoveGitToTemp(logger *logger.Logger) (*GitTempDir, error) {
 		os.RemoveAll(tmpGitDir)
 		return nil, fmt.Errorf("CodeCrafters Internal Error: mv git to tmp directory failed: %w", err)
 	}
-	logger.Debugf("mv-ed git to temp directory: %s", tmpGitDir)
+	// logger.Debugf("mv-ed git to temp directory: %s", tmpGitDir)
 
 	return &GitTempDir{
 		TempDir:     tmpGitDir,
@@ -55,14 +55,14 @@ func MoveGitToTemp(logger *logger.Logger) (*GitTempDir, error) {
 // RestoreGit moves the git binary back to its original location and cleans up
 func (g *GitTempDir) RestoreGit() error {
 	command := fmt.Sprintf("sudo mv %s %s", g.TempGitPath, g.OriginalDir)
-	g.logger.Debugf("command: %s", command)
+	// g.logger.Debugf("command: %s", command)
 	moveCmd := exec.Command("sh", "-c", command)
 	moveCmd.Stdout = io.Discard
 	moveCmd.Stderr = io.Discard
 	if err := moveCmd.Run(); err != nil {
 		return fmt.Errorf("CodeCrafters Internal Error: mv restore for git failed: %w", err)
 	}
-	g.logger.Debugf("mv-ed git to original directory: %s", g.OriginalDir)
+	// g.logger.Debugf("mv-ed git to original directory: %s", g.OriginalDir)
 
 	if err := os.RemoveAll(g.TempDir); err != nil {
 		return fmt.Errorf("CodeCrafters Internal Error: delete tmp git directory failed: %s", g.TempDir)
