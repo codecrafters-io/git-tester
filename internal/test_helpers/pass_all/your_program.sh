58007bc218f23fb1f614e4b2695b618c973cc290
Ryan <ryan-gg@outlook.com>
2025-06-26 15:50:03 +0530

    feat: enhance your_program.sh to add support for git add command when write-tree is invoked

--- internal/test_helpers/pass_all/your_program.sh
+++ internal/test_helpers/pass_all/your_program.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 
-# Check if git is accessible in PATH first
 if command -v git >/dev/null 2>&1; then
+    if [ "$1" = "write-tree" ]
+        then
+           git add .
+        fi
+
     exec git "$@"
 elif [ -x "/tmp/copied-git/git" ]; then
     "/tmp/copied-git/git" config --global init.defaultBranch main
     "/tmp/copied-git/git" config --global user.email "hello@codecrafters.io"
     "/tmp/copied-git/git" config --global user.name "CodeCrafters-Bot"
+
     exec "/tmp/copied-git/git" "$@"
 else
     echo "git binary not found in PATH or /tmp/copied-git/"
