96a1493578abcbf52b7100c3e5a25a5481be397c
Ryan <ryan-gg@outlook.com>
2025-07-07 23:02:02 +0530

    refactor: improve comments in your_program.sh for clarity and accuracy

--- internal/test_helpers/pass_all/your_program.sh
+++ internal/test_helpers/pass_all/your_program.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Check if git is working in PATH first
+# Check if git from PATH is working first
 if command -v git >/dev/null 2>&1; then
     if [ "$1" = "write-tree" ]; then
         git add .
@@ -11,11 +11,12 @@ fi
 # Find git binary in /tmp locations
 for tmpdir in /tmp/git-*/git; do
     if [ -x "$tmpdir" ]; then
+        # If defaultBranch config is not set, we set it to main (doesn't work without global config)
         if ! "$tmpdir" config --global --get init.defaultBranch >/dev/null 2>&1; then
             "$tmpdir" config --global init.defaultBranch main
         fi
 
-        # commit-tree stage doesn't use call this script for init
+        # commit-tree stage doesn't use this script for init
         # So we need to run this setup again
         if [ "$1" = "commit-tree" ]; then
             "$tmpdir" config --local user.email "hello@codecrafters.io"
